ffd9b0de7fd580c9c5e9574f3f64493bf4d4502e
MikhailKurotchenko <77405123+MikhailKurotchenko@users.noreply.github.com>
2021-01-19 22:17:59 +0300

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,19 @@
+public class Variable {
+     public static void main (String[] args) {
+        byte windowsType = 7;
+        short RAM = 4;
+        int systemType = 64;
+        long buyDate = 2016;
+        float freqCPU = 1.7f;
+        double rating = 4.7d;
+        char OS = 'W';
+        boolean isWindows = true;
+        System.out.println(windowsType);
+        System.out.println(RAM);
+        System.out.println(systemType);
+        System.out.println(buyDate);
+        System.out.println(freqCPU);
+        System.out.println(rating);
+        System.out.println(OS);
+    }
+}
