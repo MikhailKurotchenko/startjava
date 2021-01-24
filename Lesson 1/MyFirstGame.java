9a51ee9213b154d598205678aa759f63d7986c4f
MikhailKurotchenko <shaurmanaaviamotornoy@gmail.com>
2021-01-24 21:35:55 +0300

    Последние исправления

--- Lesson 1/MyFirstGame.java
+++ Lesson 1/MyFirstGame.java
@@ -3,20 +3,15 @@ public class MyFirstGame {
         int computerNum = -1;
         int userNum = 10;
         if(computerNum > 0 & computerNum <= 100) {
-            if(computerNum > userNum) {
-                while(computerNum > userNum) {
-                    System.out.println("Введенное вами число меньше того, что загадал компьютер");
-                    userNum++;
-                }
-            } else if(computerNum < userNum) {
-                while(computerNum < userNum) {
+            while(computerNum > userNum) {
+                System.out.println("Введенное вами число меньше того, что загадал компьютер");
+                userNum++;
+            }
+            while(computerNum < userNum) {
                     System.out.println("Введенное вами число больше того, что загадал компьютер");
                     userNum--;
-                }
             }
-
             System.out.println("Вы угадали!");
-
         } else {
             System.out.println("Ошибка: значение computerNum должно быть в диапазоне (0;100]");
         }
