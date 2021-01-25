1984f239991943260d1c2170511ebf82795bf6fa
MikhailKurotchenko <shaurmanaaviamotornoy@gmail.com>
2021-01-25 15:07:34 +0300

    Первое задание 2 урока

---
+++ Lesson 2/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+
+    String sex = "Male";
+    String name = "John";
+    int height = 180;
+    int weight = 75;
+    int age = 28;
+
+    void move() {
+
+    }
+
+    void sit() {
+
+    }
+
+    void run(){
+
+    }
+
+    void talk(){
+
+    }
+
+    boolean learnJava(){
+        System.out.println("You're learn a second Lesson on startjava");
+        return true;
+    }
+}
