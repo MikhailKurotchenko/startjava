62adee02948acfee640fb5a06701dcb5b37432bf
MikhailKurotchenko <shaurmanaaviamotornoy@gmail.com>
2021-02-16 20:01:35 +0300

    Исправленный Jaeger

--- Lesson 2/Jaeger.java
+++ Lesson 2/Jaeger.java
@@ -1,15 +1,14 @@
 public class Jaeger {
-    public String modelName;
-    public String mark;
-    public String origin;
-    public float height;
-    public float weight;
-    public int speed;
-    public int strengh;
-    public int armor;
+    private String modelName;
+    private String mark;
+    private String origin;
+    private float height;
+    private float weight;
+    private int speed;
+    private int strengh;
+    private int armor;
 
-    public Jaeger(String modelName, String mark, String origin,
-float height, float weight, int speed, int strengh, int armor) {
+    public Jaeger(String modelName, String mark, String origin, float height, float weight, int speed, int strengh, int armor) {
         this.modelName = modelName;
         this.mark = mark;
         this.origin = origin;
@@ -18,6 +17,38 @@ float height, float weight, int speed, int strengh, int armor) {
         this.speed = speed;
         this.strengh = strengh;
         this.armor = armor;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getStrengh() {
+        return strengh;
+    }
+
+    public int getArmor() {
+        return armor;
     }
 
     public boolean drift() {
--- Lesson 2/JaegerTest.java
+++ Lesson 2/JaegerTest.java
@@ -3,40 +3,22 @@ public class JaegerTest {
         Jaeger jager1 = new Jaeger("Gipsy Danger", "Mark-3", "USA", 79.25f, 1.980f, 7, 8, 6);
         Jaeger jager2 = new Jaeger("Coyote Tango", "Mark-1", "Japan", 85.34f, 2.312f, 5, 7, 4);
 
-        System.out.println(jager1.modelName);
-
-        System.out.println(jager1.mark);
-
-        System.out.println(jager1.origin);
-
-        System.out.println(jager1.height);
-
-        System.out.println(jager1.weight);
-
-        System.out.println(jager1.speed);
-
-        System.out.println(jager1.strengh);
-
-        System.out.println(jager1.armor);
-
-
+        System.out.println(jager1.getModelName());
+        System.out.println(jager1.getMark());
+        System.out.println(jager1.getOrigin());
+        System.out.println(jager1.getHeight());
+        System.out.println(jager1.getWeight());
+        System.out.println(jager1.getSpeed());
+        System.out.println(jager1.getStrengh());
+        System.out.println(jager1.getArmor());
         System.out.println("");
-
-
-        System.out.println(jager2.modelName);
-
-        System.out.println(jager2.mark);
-
-        System.out.println(jager2.origin);
-
-        System.out.println(jager2.height);
-
-        System.out.println(jager2.weight);
-
-        System.out.println(jager2.speed);
-
-        System.out.println(jager2.strengh);
-        
-        System.out.println(jager2.armor);
+        System.out.println(jager2.getModelName());
+        System.out.println(jager2.getMark());
+        System.out.println(jager2.getOrigin());
+        System.out.println(jager2.getHeight());
+        System.out.println(jager2.getWeight());
+        System.out.println(jager2.getSpeed());
+        System.out.println(jager2.getStrengh());
+        System.out.println(jager2.getArmor());
     }
 }
